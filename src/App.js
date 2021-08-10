c7c86a28162d38b7a43128bb8c996b2325c989c3
joshah98 <josha.heimann@alumni.ubc.ca>
2021-08-09 22:19:59 -0700

    geocoding api integration

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import {Room} from "@material-ui/icons";
 import "./app.css";
-import { SearchBar } from './components/SearchBar.js';
+import { SearchBar } from './components/Searchbar/SearchBar.js';
+import axios from 'axios';
 
-function App() {
+const App = () => {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -12,30 +13,74 @@ function App() {
     longitude: -123.2460,
     zoom: 14
   });
-  
+
+  const [pins, addPin] = useState([]);
+  const [markers, addMarker] = useState([]);
 
   const [showPopup, togglePopup] = useState(true);
 
+  const addNewMarker = (course, section, building, time) => {
+    let newMarker = {
+      course: course,
+      section: section,
+      building: building,
+      time: time
+    };
+
+    addMarker([...markers, newMarker]);
+  };
+
+  const addNewPin = (course, section) => {
+    axios.get(`https://ubcapi.herokuapp.com/courses/${course}/${section}`)
+      .then(result => {
+        let building = result.data[0].building;
+        let time = result.data[0].time;
+
+        addNewMarker(course, section, building, time);
+
+        axios.get(`http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_GEOCODE_TOKEN}&query=${building} vancouver`)
+          .then(result => {
+            let newPin = {
+              latitude: result.data.data[0].latitude,
+              longitude: result.data.data[0].longitude
+            };
+
+            addPin([...pins, newPin]);
+          })
+      });
+
+      setViewport(viewport);
+
+  };
+
   return (
     <div className="app">
-      <SearchBar/>        
+      <SearchBar newPin={(course, section) => addNewPin(course, section) }/>        
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
         onViewportChange={nextViewport => setViewport(nextViewport)}
         mapStyle="mapbox://styles/joshah/ckrfy2ocs2v4x18lfk22f06cu"
         >
-        <Marker latitude={49.2606} longitude={-123.246} offsetLeft={-20} offsetTop={-10}>
-          <Room style={{fontSize: viewport.zoom * 3}}/>
-        </Marker>
+
+        <div className="markers">
+          {pins.map((p, i) =>            
+            <Marker latitude={p.latitude} longitude={p.longitude} offsetLeft={-20} offsetTop={-10} key={i}>
+              <Room style={{fontSize: viewport.zoom * 3}}/>
+            </Marker>
+          )}
+        </div>
+
         {/* {showPopup && <Popup
-          latitude={49.2606}
-          longitude={-123.2460}
+          latitude={49.264208}
+          longitude={-123.253054}
           closeButton={true}
           closeOnClick={false}
           onClose={() => togglePopup(false)}
           anchor="left" >
           <div className="checkpoint">
+          <label>Course</label>
+          <h4 className="course">CHEM 111 101</h4>
           <label>Building</label>
           <h4 className="content">UBC building</h4>
           <label>Time</label>
@@ -45,6 +90,6 @@ function App() {
       </ReactMapGL>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -1,18 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import "./searchbar.css";
+import { Button } from '@material-ui/core';
 
-export const SearchBar = () => {
+export const SearchBar = props => {
     const [courses, setCourses] = useState([]);
     const [codeText, setCourseText] = useState('');
-    const [sectionText, setSectionText] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-    const [sectionSuggestions, setSectionSuggestions] = useState([]);
     const [sections, setSections] = useState([]);
     const [course, setCourse] = useState('');
     const [section, setSection] = useState('');
+    const [error, setError] = useState(false);
     const dropdownLen = 15;
-
+    
     useEffect(() => {
         const loadCourses = async() => {
             axios.get('https://ubcapi.herokuapp.com/courses/codes')
@@ -34,17 +34,20 @@ export const SearchBar = () => {
         // Find all the sections for the selected course
         axios.get(`https://ubcapi.herokuapp.com/courses/${courseCode}/sections`)
         .then((result) => {
-            setSections(result.data);
-            console.log(result.data);
+            if (result.data.length > 0) {
+                setSections(result.data);
+                setError(false);
+            } else {
+                setSections([]);
+                setError(true);
+            }
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            setSections([]);
+            console.log(err);
+            setError(true);
+        });
     };
-
-    const onSectionSuggestHandler = (text) => {
-        setSectionText(text);
-        setSectionSuggestions([]);
-        setSection(text);
-    } 
     
     const courseMatches = (text) => {
         let matches = []
@@ -59,39 +62,37 @@ export const SearchBar = () => {
         setCourseText(text);
     };
     
-    const sectionMatches = (text) => {
-        let matches = []
-        
-        if (text.length > 0) {
-            matches = sections.filter(item => {
-                const regex = new RegExp(`^${text}`, "gi");
-                return item.match(regex);
-            })
-        }
-        setSectionSuggestions(matches);
-        setSectionText(text);
+    const selectSection = (section) => {
+        setSection(section);
     };
 
     return (
         <div className='container'>
-            <input 
-                className='codes'
-                type='text' 
-                onChange={e => courseMatches(e.target.value)}
-                value={codeText}
-                onBlur={() => {
-                    setTimeout(() => {setSuggestions([])}, 100);
-                }}
+            <form onSubmit={(event) => {
+                onSuggestHandler(codeText); 
+                event.preventDefault();}
+                }>
+                <input
+                    type='text' 
+                    onChange={e => courseMatches(e.target.value)}
+                    value={codeText}
+                    onBlur={() => {
+                        setTimeout(() => {setSuggestions([])}, 100);
+                    }}
                 />
+                <input type="submit" value="Find sections"/>
+            </form>
+            {error &&
+                <h1 className='error'>This course code is either invalid or doesn't exist</h1>
+            }
             <ul>
                 {suggestions && suggestions.map((suggestion, i) => 
                     <li className='dropdown' key={i} onMouseDown={(e) => e.preventDefault()} onClick={() => {onSuggestHandler(suggestion);}}>{suggestion}</li>
                     )}
             </ul>
 
-            <input
+            {/* <input
                 type='text'
-                className='sections'
                 onChange={e => sectionMatches(e.target.value)}
                 value={sectionText}
                 onBlur={() => {
@@ -102,7 +103,24 @@ export const SearchBar = () => {
                 {sectionSuggestions && sectionSuggestions.map((suggestion, i) => 
                     <li className='dropdown' key={i} onMouseDown={(e) => e.preventDefault()} onClick={() => {onSectionSuggestHandler(suggestion);}}>{suggestion}</li>
                 )}
-            </ul>
+            </ul> */}
+            <form onSubmit={(event) => {
+                    event.preventDefault();
+                    props.newPin(course, section);
+                }
+            }>
+                {sections.length > 0 &&
+                    <>            
+                        <div className='sections'>
+                            {sections.map((sec, i) => 
+                                <div className='section' key={i} tabIndex={`${i}`} onClick={() => {selectSection(sec)}}>{sec}</div>
+                            )}
+                        </div>
+                    </>
+                }
+                <input type="submit" value="Add Course"/>
+            </form>
+            
         </div>
     )
 };

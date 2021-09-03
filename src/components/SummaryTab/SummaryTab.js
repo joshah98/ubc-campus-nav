6b5291db5de59b5b40038e65c5a964bf4fb95a07
joshah98 <josha.heimann@alumni.ubc.ca>
2021-09-02 21:16:21 -0700

    added second page for summary tab

--- src/components/SummaryTab/SummaryTab.js
+++ src/components/SummaryTab/SummaryTab.js
@@ -13,49 +13,68 @@ const SummaryTab = props => {
 
     const findNearby = (building) => {
         // Find all places near the building and switch to page 2
+
+        setPage(2);
     };
 
     return (
         <div className="container">   
-            <Slide direction="left" in={toggle} mountOnEnter unmountOnExit>
-                <div className="summaryTab">
-                    {props.courses.length === 0 && 
-                        <div className="emptyText">
-                            Nothin to see here yet...
-                        </div>
-                    }
-                    {props.courses.map((course, i) =>
-                        <div key={i}>
-                            {course.show === true &&
-                                <>                                
-                                    <div className="course name">
-                                        {course.course} {course.section} from {course.start} to {course.end}
+            <Slide direction="left" in={toggle} timeout={{enter:300, exit:0}} mountOnEnter unmountOnExit>
+                    <div className="summaryTab"> 
+                        <Slide direction="right" in={page === 1} timeout={{enter:300, exit:0}} mountOnEnter unmountOnExit>
+                            <div className="pageOne">
+                                {props.courses.length === 0 && 
+                                    <div className="emptyText">
+                                        Nothin to see here yet...
+                                    </div>
+                                }
+                                {props.courses.map((course, i) =>
+                                    <div key={i}>
+                                        {course.show === true &&
+                                            <>                                
+                                                <div className="course name">
+                                                    {course.course} {course.section} from {course.start} to {course.end}
 
-                                    </div>
-                                    <div className="course remove" onClick={() => props.remove(course)}>Remove Course</div>    
-                                </>                            
-                            }
-                            {course.show === false &&
-                                <div className="filler" onClick={() => findNearby(course.prevCourseBuilding)}>
-                                    <div>
-                                        Break from {course.start} to {course.end}
-                                    </div>
-                                    <div>
-                                        Click for nearby study spots :D
-                                    </div>
+                                                </div>
+                                                <div className="course remove" onClick={() => props.remove(course)}>Remove Course</div>    
+                                            </>                            
+                                        }
+                                        {course.show === false &&
+                                            <div className="filler" onClick={() => findNearby(course.prevCourseBuilding)}>
+                                                <div>
+                                                    Break from {course.start} to {course.end}
+                                                </div>
+                                                <div>
+                                                    Click for nearby study spots :D
+                                                </div>
+                                            </div>
+                                        }
+                                    </div> 
+                                )}
+                            </div>
+                        </Slide>
+
+                        <Slide direction="left" in={page === 2} timeout={{enter:300, exit:0}} mountOnEnter unmountOnExit>
+                            <div className="pageTwo">
+                                <div onClick={() => setPage(1)}>
+                                    <AiOutlineArrowLeft className="back"/>
                                 </div>
-                            }
-                        </div> 
-                    )}
-                    <div className="toggleoff" onClick={() => changeToggle()}>
-                        <AiOutlineArrowRight className="arrow"/>
-                    </div>
+                                <div className="locations">
+                                    List of nearby study spots goes here ...
+                                </div>
 
-                </div>
+                            </div>
+                        </Slide>
+                        
+
+                        <div className="toggleoff" onClick={() => changeToggle()}>
+                            <AiOutlineArrowRight className="arrow"/>
+                        </div>
+                    </div>
             </Slide>
                  
 
-            <Slide direction="left" in={!toggle} mountOnEnter unmountOnExit>
+            <Slide direction="left" in={!toggle} timeout={{enter:200, exit:0}} mountOnEnter unmountOnExit>
                 <div className="toggleon" onClick={() => changeToggle()}>
                     <AiOutlineArrowLeft className="arrow"/>
                 </div>
